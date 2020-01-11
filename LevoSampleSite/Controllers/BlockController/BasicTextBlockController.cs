2e1927a6db2d55f2d210da1fdc9e61bf3b6b1108
Dan-Cl <Dan.Clissold@protonmail.com>
2020-01-11 17:27:10 +1100

    adds basic text block mvc

---
+++ LevoSampleSite/Controllers/BlockController/BasicTextBlockController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Web;
+using EPiServer.Web.Mvc;
+using LevoSampleSite.Models.Blocks;
+
+namespace LevoSampleSite.Controllers.BlockController
+{
+    public class BasicTextBlockController : BlockController<BasicTextBlockDataModel>
+    {
+        public override ActionResult Index(BasicTextBlockDataModel currentBlock)
+        {
+            return PartialView("BasicTextBlock", new BasicTextBlockViewModel(currentBlock));
+        }
+    }
+}
