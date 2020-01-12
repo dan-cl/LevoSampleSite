73936dd8f0c70c5c8a6f0d80b7f71a7b2b754f04
Dan-Cl <Dan.Clissold@protonmail.com>
2020-01-12 16:58:45 +1100

    adds api controller

--- LevoSampleSite/Global.asax.cs
+++ LevoSampleSite/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Web.Mvc;
+using System.Web.Http;
+using System.Web.Routing;
 
 namespace LevoSampleSite
 {
@@ -8,6 +10,7 @@ namespace LevoSampleSite
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
 
             //Tip: Want to call the EPiServer API on startup? Add an initialization module instead (Add -> New Item.. -> EPiServer -> Initialization Module)
         }
